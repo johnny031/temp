4bc7c7ba4a0a64cadc89b03d4d4492878ce13dd5
Johnny <johnny055238@gmail.com>
2020-07-11 22:42:11 +0800

    adjust members' grid system to flex box

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -12,14 +12,6 @@ $(document).ready(function () {
   } else {
     rm_nav_white();
   }
-  // dropdown animation
-  $(".dropdown").on("show.bs.dropdown", function (e) {
-    $(this).find(".dropdown-menu").first().stop(true, true).slideDown(250);
-  });
-  $(".dropdown").on("hide.bs.dropdown", function (e) {
-    let time = $(window).width() < 768 ? 0 : 250;
-    $(this).find(".dropdown-menu").first().stop(true, true).slideUp(time);
-  });
   //change nav background color on click when collapsed
   $(".navbar-toggle").on("click", function () {
     let expand = $(".navbar-toggle").attr("aria-expanded");
@@ -35,20 +27,6 @@ $(document).ready(function () {
   //hide breadcrumb at home page
   if (window.location.pathname == "/") {
     $("#breadcrumb").hide();
-  }
-  //wrap members in rows
-  let $set1 = $(".directors_list").children();
-  let $set2 = $(".supervisors_list").children();
-  let $set3 = $(".secretaries_list").children();
-  let col_num = $(window).width() < 992 ? 2 : 3;
-  for (let i = 0, len = $set1.length; i < len; i += col_num) {
-    $set1.slice(i, i + col_num).wrapAll('<div class="row"></div>');
-  }
-  for (let i = 0, len = $set2.length; i < len; i += col_num) {
-    $set2.slice(i, i + col_num).wrapAll('<div class="row"></div>');
-  }
-  for (let i = 0, len = $set3.length; i < len; i += col_num) {
-    $set3.slice(i, i + col_num).wrapAll('<div class="row"></div>');
   }
 });
 $(window).scroll(function () {
